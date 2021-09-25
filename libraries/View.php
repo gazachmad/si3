62c6f41009f6469b626ee7f82c5f29774e9afd20
Gaza <gazachmad@gmail.com>
2021-09-25 12:11:05 +0800

    Blade library

---
+++ libraries/View.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Lib;
+
+use Illuminate\Container\Container;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\View\Compilers\BladeCompiler;
+use Illuminate\View\Engines\CompilerEngine;
+use Illuminate\View\Engines\EngineResolver;
+use Illuminate\View\Factory;
+use Illuminate\View\FileViewFinder;
+
+class View extends Factory
+{
+    public function __construct()
+    {
+        parent::__construct(new EngineResolver, new FileViewFinder(new Filesystem, [VIEWPATH]), new Dispatcher(new Container));
+
+        $this->_initialize();
+    }
+
+    private function _initialize()
+    {
+        parent::addExtension('blade.php', 'blade', function () {
+            return new CompilerEngine(new BladeCompiler(new Filesystem, FCPATH . 'bootstrap/Cache'));
+        });
+    }
+
+    public function make($view, $data = [], $mergeData = [])
+    {
+        echo parent::make($view, $data, $mergeData);
+    }
+}
